bf7cdf17a809b0eb7ab4bb65dbc2631c95faeb14
Aminbaklouti <87398130+Aminbaklouti@users.noreply.github.com>
2023-09-03 22:33:06 +0100

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,36 @@
+
+function scrollToHome(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+}
+
+function scrollToAbout(){
+    window.scrollTo({
+        top: 1000,
+        behavior: "smooth",
+    });
+}
+
+function scrollToSpeakers(){
+    window.scrollTo({
+        top: 2100,
+        behavior: "smooth",
+    });
+}
+
+function scrollToSchedules(){
+    window.scrollTo({
+        top: 3200,
+        behavior: "smooth",
+    });
+}
+function scrollToContact(){
+    window.scrollTo({
+        top: 7480,
+        behavior: "smooth",
+    });
+}
+
+$('.counter').countUp();
